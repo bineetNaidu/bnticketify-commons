6b146e1f6d6c476de7b3f717db2302b8d6bfecd6
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-09 22:13:36 +0530

    add NATS custom class

---
+++ src/events/TicketUpdatedEvent.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './Subjects';
+
+export interface TicketUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
