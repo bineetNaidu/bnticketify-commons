6d6742cd1ba7fa1859c506e4eb77109d1ee80834
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-27 12:56:55 +0530

    add new event interfaces

---
+++ src/events/OrderCancelledEvent.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './Subjects';
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
---
+++ src/events/OrderCreatedEvent.ts
@@ -0,0 +1,16 @@
+import { Subjects } from './Subjects';
+import { OrderStatus } from './types/orderStatus';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
