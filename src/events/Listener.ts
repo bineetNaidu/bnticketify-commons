68de561127e0f2c915ff08673ecaba4d69a68a6a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-30 12:50:46 +0530

    updated listeners and add OrderId type difinations

--- src/events/Listener.ts
+++ src/events/Listener.ts
@@ -7,7 +7,7 @@ interface Event {
 }
 
 export abstract class Listener<T extends Event> {
-  private client: Stan;
+  protected client: Stan;
   abstract subject: T['subject'];
   abstract queueGroupName: string;
   abstract onMessage(data: T['data'], msg: Message): void;
