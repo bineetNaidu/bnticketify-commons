d00da716193aae77af3149689cc8fc5e7ad80a15
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-03 21:54:02 +0530

    add PaymentCreatedEvent event

---
+++ src/events/PaymentCreatedEvent.ts
@@ -0,0 +1,10 @@
+import { Subjects } from './Subjects';
+
+export interface PaymentCreatedEvent {
+  subject: Subjects.PaymentCreated;
+  data: {
+    id: string;
+    orderId: string;
+    stripeId: string;
+  };
+}
